89634ffbe0c20785bc457f075fe110b0340f6274
Rafael <rafalyf11@gmail.com>
2022-05-09 19:06:06 -0300

    feat req 3: implementada validacao de nome e quantidade

---
+++ middlewares/validateProductsMdw.js
@@ -0,0 +1,32 @@
+const validateName = (req, res, next) => {
+  try {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ message: '"name" is required' });
+    if (name.length < 5) {
+      return res.status(422)
+      .json({ message: '"name" length must be at least 5 characters long' }); 
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+const validateQuantity = (req, res, next) => {
+  try {
+    const { quantity } = req.body;
+    if (!quantity) return res.status(400).json({ message: '"quantity" is required' });
+    if (quantity <= 0) {
+      return res.status(422)
+      .json({ message: '"quantity" must be greater than or equal to 1' }); 
+    }
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = {
+  validateName,
+  validateQuantity
+};
--- routes/products/index.js
+++ routes/products/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { allProducts, productById, insertProduct } = require('../../controllers/productsController');
+const { validateName, validateQuantity } = require('../../middlewares/validateProductsMdw');
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', allProducts);
 productsRouter.get('/:id', productById);
-productsRouter.post('/', insertProduct);
+productsRouter.post('/', validateName, validateQuantity, insertProduct);
 
 module.exports = productsRouter;

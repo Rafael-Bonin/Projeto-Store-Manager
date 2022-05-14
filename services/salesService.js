ef34fb42849a914f1657ea71ace98bc7a55dca38
Rafael <rafalyf11@gmail.com>
2022-05-14 20:44:56 -0300

    fix: arrumado problema de moch na services sales test

--- services/salesService.js
+++ services/salesService.js
@@ -1,37 +1,35 @@
-const { getAllSale, saleById, addSale, updateSale,
-  deleteSale,
-  getSaleProducts } = require('../models/salesModel');
+const models = require('../models/salesModel');
 
 const getAll = async () => {
-  const all = await getAllSale();
+  const all = await models.getAllSale();
   return all;
 };
 
 const byId = async (id) => {
-  const ids = await saleById(id);
+  const ids = await models.saleById(id);
   if (ids.length === 0) throw new Error('Sale not found');
   return ids;
 };
 
 const createSale = async (array) => {
-  const add = await addSale(array);
+  const add = await models.addSale(array);
   return add;
 };
 
 const editSale = async (id, productId, quantity) => {
-  const update = await updateSale(id, productId, quantity);
+  const update = await models.updateSale(id, productId, quantity);
   return update;
 };
 
 const removeSale = async (id) => {
-  const ids = await saleById(id);
+  const ids = await models.saleById(id);
   if (ids.length === 0) throw new Error('Sale not found');
-  const result = deleteSale(id);
+  const result = models.deleteSale(id);
   return result;
 };
 
 const productsSales = async (id) => {
-  const result = await getSaleProducts(id);
+  const result = await models.getSaleProducts(id);
   return result;
 };
 
